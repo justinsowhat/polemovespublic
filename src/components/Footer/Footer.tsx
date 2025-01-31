b0f421b4f5cd80a5e85d798bc039018e689cc586
Justin <justinsowhatnow@gmail.com>
2025-01-31 09:41:27 -0800

    Updates

--- src/components/Footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export const Footer = () => {
-  return (
-    <footer>
-      <p>
-        &copy; {new Date().getFullYear()} Impolester LLC. All rights reserved.
-      </p>
-    </footer>
-  );
-};
